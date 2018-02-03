d736f1e9c190ddbbb0c27f69796f13e0afed03b5
schemr <schemryj@gmail.com>
2018-02-03 19:19:12 +0900

    Added header UI handling by state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Header.scss';
 
-const header = () => (
-    <header className={classes.Header}>
-        <div className={classes.Title}>Good Job!</div>
-        {/* <nav className={classes.Nav}>
-            <ul>
-                <li>Logout</li>
-            </ul>
-        </nav> */}
-    </header>
-);
+class Header extends Component {
+    state = {
+        isAuth: false
+    }
+    render() {
+        return(
+            <header className={classes.Header}>
+                {this.state.isAuth ? "List" : null}
+                <div className={classes.Title}>Good Job!</div>
+                {this.state.isAuth ? <nav className={classes.Nav}>
+                    <ul>
+                        <li>Logout</li>
+                    </ul>
+                </nav> : null}
+            </header>
+        )
+    }
+    
+}
 
-export default header;
+export default Header;
