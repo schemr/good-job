4174032d380bffd7b7307653958c8670d4cddc90
schemr <schemryj@gmail.com>
2018-02-07 18:35:47 +0900

    Change Spinner component styling

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -2,11 +2,7 @@ import React from 'react';
 import classes from './Spinner.scss';
 
 const spinner = () => (
-    <div className={classes.Spinner}>
-        <div className={classes.bounce1}></div>
-        <div className={classes.bounce2}></div>
-        <div className={classes.bounce3}></div>
-    </div>
+    <div className={classes.Spinner}></div>
 );
 
 export default spinner; 
