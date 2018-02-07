0277d4369a568b727007c49331aa6386ca8fe307
schemr <schemryj@gmail.com>
2018-02-07 18:35:10 +0900

    Change Spinner component

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -2,7 +2,11 @@ import React from 'react';
 import classes from './Spinner.scss';
 
 const spinner = () => (
-    <div className={classes.Loader}>Loading...</div>
+    <div className={classes.Spinner}>
+        <div className={classes.bounce1}></div>
+        <div className={classes.bounce2}></div>
+        <div className={classes.bounce3}></div>
+    </div>
 );
 
 export default spinner; 
