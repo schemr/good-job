4da86d0b0ea5bbb92142a460fb07190f996ea2a9
schemr <schemryj@gmail.com>
2018-02-07 16:30:36 +0900

    Initial reducer, store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import authReducer from './store/reducers/auth';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+    auth: authReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware()));
+
 const appRouter = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(appRouter, document.getElementById('root'));
