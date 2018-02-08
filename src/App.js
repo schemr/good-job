00be977ec86e809c688aa89961207fedc598ff2f
schemr <schemryj@gmail.com>
2018-02-08 15:56:58 +0900

    Routing according to auth logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import Introduce from './components/Introduce/Introduce';
 import Sentences from './components/Sentences/Sentences';
@@ -8,17 +9,43 @@ import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={Introduce} />
+        <Redirect to="/" />
+      </Switch>
+    )
+    if(this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/sentences" component={Sentences} />
+          <Route path="/new" component={NewSentence} />  
+          <Redirect to="/new" />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Route path="/sentences" component={Sentences} />
-          <Route path="/new" component={NewSentence} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/" exact component={Introduce} />  
+          {routes}
         </Layout>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // onTryAutoSignup: () => dispatch( actions.authCheckState() )
+  };
+};
+
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
+
