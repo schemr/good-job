425c5aa74876d4196d7cfe9a86475cd83be16409
schemr <schemryj@gmail.com>
2018-02-13 18:09:58 +0900

    Modify introduce component link button

--- src/components/Introduce/Introduce.js
+++ src/components/Introduce/Introduce.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Introduce.scss';
 import { Link } from 'react-router-dom';
+import Button from '../UI/Button/Button';
 
 const introduce = (props) => (
     <div className={classes.Container}>
@@ -8,10 +9,9 @@ const introduce = (props) => (
         잘한일을 써보세요
         어제보다 오늘, 반드시 더 좋아집니다. 잘한일을 쓰는 것만으로도!
         </div>
-        <Link 
-            className={classes.Button} 
-            to="/auth"
-            >시작하기</Link>
+        <Link to="/auth">
+            <Button>시작하기</Button>
+        </Link>
     </div>
 )
 
