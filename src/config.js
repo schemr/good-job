a25b9d45999e9f4798ef76c25d9ea5dc0148b2d1
schemr <schemryj@gmail.com>
2018-02-17 19:32:01 +0900

    Remove unnecessary key

--- src/config.js
+++ src/config.js
@@ -2,8 +2,6 @@ const config = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     databaseURL: process.env.REACT_APP_DB_URL,
     storageBucket: process.env.REACT_APP_STRG_BKT,
-    messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
-    embedlyKey : process.env.REACT_APP_EMBEDLY_KEY
+    messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID
 };
-
 export default config;
