09f534d88064fe8267cefbdc1f60426096ff0548
schemr <schemryj@gmail.com>
2018-02-20 21:52:29 +0900

    Added firebase config project Id

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    projectId: process.env.REACT_APP_PRO_ID,
     databaseURL: process.env.REACT_APP_DB_URL,
     storageBucket: process.env.REACT_APP_STRG_BKT,
     messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID
