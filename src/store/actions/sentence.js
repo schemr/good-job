bfb1baedc7ba7d1cb5154cb77fa7027e8e37e6ce
schemr <schemryj@gmail.com>
2018-02-27 16:24:32 +0900

    Change sentences list limit, annotation remove

--- src/store/actions/sentence.js
+++ src/store/actions/sentence.js
@@ -5,7 +5,6 @@ export const addSentence = (sentence, userId, token) => {
     return dispatch => {
         dispatch(uiStartLoading());
         console.log(sentence)
-        // Todo Data structure change 
         fetch('https://good-job-ff4ca.firebaseio.com/sentences/'+userId+'/'+sentence.displayDate+'.json?auth='+token, {
                     method:"POST",
                     body: JSON.stringify(sentence),
@@ -28,8 +27,7 @@ export const addSentence = (sentence, userId, token) => {
 
 export const getSentences = (token, userId) => {
     return dispatch => {
-        const queryParams = '?auth=' + token +'&orderBy="$key"&limitToLast=5';
-        // Todo Data structure change 
+        const queryParams = '?auth=' + token +'&orderBy="$key"&limitToLast=10';
         fetch("https://good-job-ff4ca.firebaseio.com/sentences/"+userId+".json"+queryParams)
             .then(res => res.json())
             .then(data => {
