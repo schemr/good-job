42be03696993cb69af3ce83a3d271e45eebdf34a
schemr <schemryj@gmail.com>
2018-02-27 17:04:40 +0900

    Change add button font

--- src/components/UI/AddButton/AddButton.js
+++ src/components/UI/AddButton/AddButton.js
@@ -3,7 +3,7 @@ import classes from './AddButton.scss';
 
 const addButton = (props) => (
     <div className={classes.addButton}>
-        <button onClick={props.addedButton}>+</button>
+        <button onClick={props.addedButton}><i className="fa fa-2x fa-plus"></i></button>
     </div>
 );
 
