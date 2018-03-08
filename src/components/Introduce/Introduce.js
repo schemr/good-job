58ff33913e60ad70d931adde0aa46240b9efb944
schemr <schemryj@gmail.com>
2018-03-08 18:59:29 +0900

    Adjust material design introduce component

--- src/components/Introduce/Introduce.js
+++ src/components/Introduce/Introduce.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Introduce.scss';
 import { Link } from 'react-router-dom';
-import Button from '../UI/Button/Button';
+import Button from 'material-ui/Button';
 
 const introduce = (props) => (
     <div className={classes.Container}>
@@ -9,8 +9,10 @@ const introduce = (props) => (
         잘한일을 써보세요
         어제보다 오늘, 반드시 더 좋아집니다. 잘한일을 쓰는 것만으로도!
         </div>
-        <Link to="/auth">
-            <Button>시작하기</Button>
+        <Link to="/auth" className={classes.BtnWrapper}>
+            <Button variant="raised" color="primary">
+                시작하기
+            </Button>
         </Link>
     </div>
 )
