50736871d33acb5b1349e568cad60134f7ce3ae4
schemr <schemryj@gmail.com>
2018-03-12 17:20:23 +0900

    Adjust tool bar material ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './Header.scss';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography'
 
 const header = (props) => (
-    <header className={classes.Header}>
-        {props.isAuth ? <NavLink to="/sentences"><i className="fa fa-list-ul"></i></NavLink> : null}
+    <div>
+        <AppBar position="static" color="primary" className={classes.Header}>
+            <Toolbar>
+                { props.isAuth && (
+                    <NavLink to="/sentences"><i className="fa fa-list-ul"></i></NavLink>
+                )}
+                <Typography variant="title" color="inherit" className={classes.Title}>
+                    Good Job!
+                </Typography>
+                { props.isAuth && (
+                    <nav className={classes.Nav}>
+                        <ul>
+                            <li><NavLink to="/logout"><i className="fa fa-sign-out-alt"></i></NavLink></li>
+                        </ul>
+                    </nav>
+                )}
+            </Toolbar>
+        </AppBar>
+        {/* {props.isAuth ? <NavLink to="/sentences"><i className="fa fa-list-ul"></i></NavLink> : null}
         <div className={classes.Title}>Good Job!</div>
         {props.isAuth ? <nav className={classes.Nav}>
             <ul>
                 <li><NavLink to="/logout"><i className="fa fa-sign-out-alt"></i></NavLink></li>
             </ul>
-        </nav> : null}
-    </header>
+        </nav> : null} */}
+    </div>
 );
 
 export default header;
