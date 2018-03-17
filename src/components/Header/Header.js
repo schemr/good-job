f0f678c5a857dedf75dd705af1fa366e78bb3ba4
schemr <schemryj@gmail.com>
2018-03-17 15:40:52 +0900

    Added Alarm button and notification permission test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
+import firebase from 'firebase';
 
 const styles = {
     Appbar: {
@@ -16,24 +17,43 @@ const styles = {
         fontWeight: "bold"
     }
   };
-  
 
-const header = (props) => (
-    <div>
-        <AppBar color="primary" className={props.classes.Appbar}>
-            <Toolbar>
-                { props.isAuth && (
-                    <NavLink to="/sentences"><i className="fa fa-list-ul"></i></NavLink>
-                )}
-                <Typography variant="title" color="inherit" className={props.classes.Title}>
-                    Good Job!
-                </Typography>
-                { props.isAuth && (
-                    <NavLink to="/logout"><i className="fa fa-sign-out-alt"></i></NavLink>
-                )}
-            </Toolbar>
-        </AppBar>
-    </div>
-);
+class Header extends Component {
+    onPushHandler = (e) => {
+        e.preventDefault();
+        const messaging = firebase.messaging();
+        messaging.requestPermission()
+        .then(function() {
+            console.log('Notification permission granted.');
+            // TODO(developer): Retrieve an Instance ID token for use with FCM.
+            // ...
+        })
+        .catch(function(err) {
+            console.log('Unable to get permission to notify.', err);
+        });
+    }
 
-export default withStyles(styles)(header);
+    render() {
+        return (
+            <div>
+                <AppBar color="primary" className={this.props.classes.Appbar}>
+                    <Toolbar>
+                        { this.props.isAuth && (
+                            <a href="#" onClick={this.onPushHandler}>
+                                <i className="fa fa-bell"></i>
+                            </a>
+                        )}
+                        <Typography variant="title" color="inherit" className={this.props.classes.Title}>
+                            Good Job!
+                        </Typography>
+                        { this.props.isAuth && (
+                            <NavLink to="/logout"><i className="fa fa-sign-out-alt"></i></NavLink>
+                        )}
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+};
+
+export default withStyles(styles)(Header);
