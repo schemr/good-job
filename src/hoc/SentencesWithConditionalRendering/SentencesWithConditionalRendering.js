c6ac2cc38c1a8eaa4c0a9e675e6b24b0cae16f8d
schemr <schemryj@gmail.com>
2018-03-24 17:44:44 +0900

    Testing HOC

--- src/hoc/SentencesWithConditionalRendering/SentencesWithConditionalRendering.js
+++ src/hoc/SentencesWithConditionalRendering/SentencesWithConditionalRendering.js
@@ -1,3 +1,18 @@
+import React from 'react';
+
 // SentencesWithConditionalRendering
 
 // Null / Empty / Loading State Conditional
+
+const SentencesWithConditionalRendering = (component) => (props) => {
+    if(!props.sentences) {
+        return null
+    }
+    if(!props.sentences.length){
+        return (<div>No Sentences</div>)
+    }
+    return <Component {...props} />
+    
+};
+
+export default SentencesWithConditionalRendering;
---
+++ src/hoc/WithAuthentication/WithAuthentication.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WithAuthentiation extends Component {
+    render() {
+        return (
+            <Component />
+        )
+    }
+};
+
+export default WithAuthentiation;
